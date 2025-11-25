730cfdff93b1b804fd9815a90b2976764b99cd67
huyluu-levinci <huy.luu@levincigroup.com>
2025-11-25 16:20:31 +0700

    Removed more unnecessary files

--- src/resources/index.ts
+++
@@ -1,6 +0,0 @@
-import { server } from '../server';
-
-export const registerAllResource = () => {
-  // Register MCP resources here
-  // Example: server.resource('example', {}, async () => { ... });
-};
